17aafbeaf93906526a0f0af58387cfb965b65140
RubenFern <rubenferval@hotmail.com>
2025-08-21 01:14:39 +0200

    refactor scripts

--- output/account-service-scripts_dml_02-insert-account.sql
+++ output/account-service-scripts_dml_02-insert-account.sql
@@ -1,4 +1,4 @@
-INSERT INTO Account (code, gender_id, username, first_name, last_name, city, state, country, biography, date_of_birth, weight, height, private, profile_picture)
+INSERT INTO Account (code, gender_id, username, first_name, last_name, city, state, country, biography, date_of_birth, weight, height, private)
 VALUES
-    ('e0533df9-1c00-4308-a1fe-edb7ab0215d2', 1, 'test', 'John', 'Doe', 'New York', 'NY', 'USA', 'Software Developer', '1990-01-01', 70.5, 175.0, FALSE, 'profile.jpg'),
-    ('06d1d7d1-9c32-4d15-9765-0c3692409fe3', 2, 'test2', 'Jane', 'Smith', 'Los Angeles', 'CA', 'USA', 'Graphic Designer', '1992-02-02', 60.0, 165.0, TRUE, 'profile2.jpg');
+    ('e0533df9-1c00-4308-a1fe-edb7ab0215d2', 1, 'test', 'John', 'Doe', 'New York', 'NY', 'USA', 'Software Developer', '1990-01-01', 70.5, 175.0, FALSE),
+    ('06d1d7d1-9c32-4d15-9765-0c3692409fe3', 2, 'test2', 'Jane', 'Smith', 'Los Angeles', 'CA', 'USA', 'Graphic Designer', '1992-02-02', 60.0, 165.0, TRUE);
--- output/account-service-scripts_dml_03-insert-account-follow.sql
+++ output/account-service-scripts_dml_03-insert-account-follow.sql
@@ -1,4 +1,4 @@
-INSERT INTO AccountFollow ( follower_account_id, followed_account_id)
+INSERT INTO AccountFollow (follower_account_id, followed_account_id)
 VALUES
     (1, 2),
     (2, 1);
